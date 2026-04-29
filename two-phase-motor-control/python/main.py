fbeca9fcdcca258daae7a12f660476642f5f076d
gjaybell <71423383+gjaybell@users.noreply.github.com>
2026-04-29 09:18:35 -0400

    Update main.py

--- two-phase-motor-control/python/main.py
+++ two-phase-motor-control/python/main.py
@@ -1,1 +1,26 @@
+from arduino.app_utils import App, Bridge
+import time
+
+def loop():
+    print("Forward slow")
+    Bridge.call("set_motor", 80)
+    time.sleep(3)
+
+    print("Forward faster")
+    Bridge.call("set_motor", 150)
+    time.sleep(3)
+
+    print("Stop")
+    Bridge.call("set_motor", 0)
+    time.sleep(2)
+
+    print("Reverse")
+    Bridge.call("set_motor", -120)
+    time.sleep(3)
+
+    print("Stop")
+    Bridge.call("set_motor", 0)
+    time.sleep(2)
+
+App.run(user_loop=loop)
 
