245e94cfd641b4fe4f66faada0470b808b9514b7
gjaybell <71423383+gjaybell@users.noreply.github.com>
2026-04-29 09:30:07 -0400

    Update sketch.ino

--- two-phase-motor-control/sketch/sketch.ino
+++ two-phase-motor-control/sketch/sketch.ino
@@ -1,1 +1,40 @@
+#include <Arduino_RouterBridge.h>
 
+const int IN1 = D3;
+const int IN2 = D4;
+
+void stop_motor() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+}
+
+void set_motor(int speed) {
+  // Limit speed
+  if (speed > 255) speed = 255;
+  if (speed < -255) speed = -255;
+
+  if (speed > 0) {
+    analogWrite(IN1, speed);   // forward
+    digitalWrite(IN2, LOW);
+  }
+  else if (speed < 0) {
+    digitalWrite(IN1, LOW);
+    analogWrite(IN2, -speed);  // reverse
+  }
+  else {
+    stop_motor();
+  }
+}
+
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  stop_motor();
+
+  Bridge.begin();
+  Bridge.provide_safe("set_motor", set_motor);
+}
+
+void loop() {
+}
