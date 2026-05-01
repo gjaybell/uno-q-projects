529dd8483747075d8dc425e1319bcba5acd27065
gjaybell <71423383+gjaybell@users.noreply.github.com>
2026-05-01 14:35:22 -0400

    Create sketch.ino

---
+++ network-motor-control-web slider/python/sketch/sketch.ino
@@ -0,0 +1,39 @@
+#include <Arduino_RouterBridge.h>
+
+const int IN1 = D3;
+const int IN2 = D5;
+
+void set_motor(int speed) {
+
+  if (speed > 255) speed = 255;
+  if (speed < -255) speed = -255;
+
+  if (abs(speed) < 20) speed = 0;
+
+  analogWrite(IN1, 0);
+  analogWrite(IN2, 0);
+  delay(50);
+
+  if (speed > 0) {
+    analogWrite(IN1, speed);
+    digitalWrite(IN2, LOW);
+  }
+  else if (speed < 0) {
+    digitalWrite(IN1, LOW);
+    analogWrite(IN2, -speed);
+  }
+  else {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+  }
+}
+
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  Bridge.begin();
+  Bridge.provide_safe("set_motor", set_motor);
+}
+
+void loop() {}
